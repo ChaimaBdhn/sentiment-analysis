ec6ece63a06463b0cab7ec8ad63f9f7817fafb11
boudehane <chaima.boudehane.etu@univ-lille.fr>
2024-10-07 00:10:04 +0200

    Updated methods

--- src/data/DataManager.java
+++ src/data/DataManager.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.regex.*;
 
 
-/* Class aiming to clean tweets */
+/* Class aiming to clean tweets from specific characters */
 public class DataManager {
 
 
@@ -16,74 +16,93 @@ public class DataManager {
     private static final String POSITIVE_EMOTICONS = ":\\)|:-\\)|:D|\\(:|<3";
     private static final String NEGATIVE_EMOTICONS = ":\\(|:'\\(|:/|\\):|\\)':";
 
-    private ArrayList<Matcher> matchersList; 
+    // private void initMatchersList(String initialTweet) {
+    //     // Creating regex patterns to compare with the given tweet
+    //     Pattern p_hashtag = Pattern.compile(HASHTAG_REGEX);
+    //     Pattern p_at = Pattern.compile(AT_REGEX);
+    //     Pattern p_rt = Pattern.compile(RT_REGEX);
+    //     Pattern p_url = Pattern.compile(URL_REGEX);
 
-
-    /**
-     *
-     */
-    public DataManager() {
-        this.matchersList = new ArrayList<>();
-    }
-
-
-    /** Initializes a list containing matchers to verify if each corresponds to the different patterns created
-     * @param initialTweet
-     * @return the matchers list
-     */
-    private void initMatchersList(String initialTweet) {
-        // Creating regex patterns to compare with the given tweet
-        Pattern p_hashtag = Pattern.compile(HASHTAG_REGEX);
-        Pattern p_at = Pattern.compile(AT_REGEX);
-        Pattern p_rt = Pattern.compile(AT_REGEX);
-        Pattern p_url = Pattern.compile(AT_REGEX);
-
-        Matcher m_hashtag = p_hashtag.matcher(initialTweet);
-        Matcher m_at = p_at.matcher(initialTweet);
-        Matcher m_rt = p_rt.matcher(initialTweet);
-        Matcher m_url = p_url.matcher(initialTweet);
+    //     Matcher m_hashtag = p_hashtag.matcher(initialTweet);
+    //     Matcher m_at = p_at.matcher(initialTweet);
+    //     Matcher m_rt = p_rt.matcher(initialTweet);
+    //     Matcher m_url = p_url.matcher(initialTweet);
         
-        this.matchersList.add(m_hashtag);
-        this.matchersList.add(m_at);
-        this.matchersList.add(m_rt);
-        this.matchersList.add(m_url);
-
-    }
+    //     this.matchersList.add(m_hashtag);
+    //     this.matchersList.add(m_at);
+    //     this.matchersList.add(m_rt);
+    //     this.matchersList.add(m_url);
+    // }
 
 
     /** Cleans a tweet from its url, @, RT, #
      * @param initialTweet
      * @return the tweet cleaned
      */
-    private String cleanTweet(String initialTweet) {
-        initMatchersList(initialTweet); // initializes a list with all matcher to compare with initial tweet
-        for(Matcher m : this.matchersList) {
-            if(m.find()) {
-                //initialTweet.replaceAll(""); todo
-            }
-        }
-    }
+    public String cleanTweet(String initialTweet) {
+        initialTweet = initialTweet.replaceAll(HASHTAG_REGEX, "");
+        initialTweet = initialTweet.replaceAll(AT_REGEX, "");
+        initialTweet = initialTweet.replaceAll(RT_REGEX, "");
+        initialTweet = initialTweet.replaceAll(URL_REGEX, "");
 
-
-    /** Deletes positive AND negative emoticons contained in a given tweet
-     * @param initialTweet
-     */
-    private String deleteEmoticons(String initialTweet) {
-        return "";
+        return initialTweet.trim(); // deletes spaces that would be caused by the replacement
     }
-
-    /** Deletes duplicated tweets :
-     * @param initialTweet
-     */
-    private String deleteDuplicatedTweets(String initialTweet) {
-        return "";
-    }
+    
 
     /** Gets the tweets written in French
      * @param initialTweet
      */
-    private String getFrenchTweets(String initialTweet) {
+    public String getFrenchTweets(String initialTweet) {
         return "";
+    }
+    
+
+    /** Deletes positive AND negative emoticons contained in a given tweet
+     * @param initialTweet given 
+     * @return the new tweet cleaned from emocticons
+     */
+    public String deleteEmoticons(String initialTweet) {
+        if(initialTweet.contains(POSITIVE_EMOTICONS) && initialTweet.contains(NEGATIVE_EMOTICONS)) {
+            initialTweet = initialTweet.replaceAll(POSITIVE_EMOTICONS, "");
+            initialTweet = initialTweet.replaceAll(NEGATIVE_EMOTICONS, "");
+        }
+        return initialTweet.trim();
+    }
+    
+    
+    /** Remove duplicated tweets by using an hashset : stores each tweet in the hashset so that it contains only unique tweets,
+     * ignoring the duplicated ones (property of hashset)
+     * @param csvFile the file containing all the tweets
+     */
+    public Set<String> removeDuplicateTweets(File csvFile) {
+        Set<String> uniqueTweets = new HashSet<>(); // where we'll store non duplicated tweets
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                String tweet = values[5]; // Each tweet is in the sixth column (index 5)
+                uniqueTweets.add(tweet);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return uniqueTweets;
+    }
+
+
+    /** 
+     * 
+     */
+    public void writeTweets(Set<String> tweets, File output) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+            for (String tweet : uniqueTweets) {
+                bw.write(tweet);
+                bw.newLine(); // New line after each tweet
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
