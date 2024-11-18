854b5b5f41db1f4e5841d1ea9910f20e1ee1c00c
chaima.boudehane.etu <chaima.boudehane.etu@univ-lille.fr>
2024-11-18 16:54:50 +0100

    Beginning Bayes classifier

--- src/algo/BayesClassifier.java
+++ src/algo/BayesClassifier.java
@@ -22,13 +22,56 @@ public class BayesClassifier extends ClassifierAlgorithm implements Polarity {
     // }
 
     
-    /**
+    /** Probabilité d'occurrence d'un mot de classe c 
      * @return
      */
-    public int occurrenceProbability() {
-        
+    public int occurrenceProbability(String word, Polarity polarity) {
+        return 0;
+
     }
 
+    /** Counts words in a sentence 
+     * @param sentence the string 
+     * @return the number of words contained in a sentence
+     */
+    public int countWords(String sentence) {
+        String[] words = sentence.trim().split("\\s");
+        return words.length;
+    }
+
+
+    /** Retrieves total number of words in a learning base
+     * @return the total number of words 
+     */
+    public int getTotalWordsOfLearningBase() {
+        int count = 0;
+        for(String tweet : this.getLearningBase().keySet()) {
+            count += this.countWords(tweet);
+        }
+        return count;
+    }
+
+
+    /**
+     * nombre total de mots des tweets de la classe c,
+     * @return
+     */
+    public int getTotalWordsByPolarity(Polarity polarity) {
+        for(String tweet : this.getLearningBase().keySet()) {
+            // if()
+        }
+
+        return 0;
+
+    }
+
+    /**  le nombre d’occurrence du mot m dans les tweets de la classe c.
+     * 
+     * @return
+     */
+    public int getNbOccurrenceWordByPolarity(String word, Polarity polarity) {
+        return 0;
+    }
 
 
 
